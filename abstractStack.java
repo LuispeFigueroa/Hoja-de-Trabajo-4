249e61ae7945a5641152d30e668654114879265c
LuisPHernandez <lphm0105@gmail.com>
2025-02-17 13:55:01 -0600

    Crear clase abstracta de Stack

--- abstractStack.java
+++ abstractStack.java
@@ -1,3 +1,20 @@
-abstract public class abstractStack implements Stack {
-    
+abstract public class abstractStack<E> implements Stack<E> {
+    /**
+     *
+     */
+    protected int count = 0;
+
+    public abstract void push(E e);
+
+    public abstract E pop();
+
+    public abstract E peek();
+
+    public boolean isEmpty() {
+        return count == 0;
+    }
+
+    public int size() {
+        return count;
+    }
 }
