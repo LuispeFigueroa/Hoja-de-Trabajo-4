e0684854605e8886df90179ac588c2f864d6fd1e
LuisPHernandez <lphm0105@gmail.com>
2025-02-18 20:25:50 -0600

    Crear interfaz para las listas

--- List.java
+++ List.java
@@ -1,3 +1,13 @@
 public interface List<E> {
-    
+    public void add(E)
+    //pre: 
+    //post: nodo con elemento se agrega a la lista
+
+    public E remove()
+    //pre: lista no esta vacía
+    //post: se devuelve el valor del último nodo y se elimina el nodo
+
+    public E getHead()
+    //pre: lista no esta vacía
+    //post: se devuelve el valor del último nodo
 }
---
+++ abstractNode.java
@@ -0,0 +1,4 @@
+public abstract class abstractNode<E> {
+    protected E valor;
+    protected abstractNode<E> siguiente;
+}
